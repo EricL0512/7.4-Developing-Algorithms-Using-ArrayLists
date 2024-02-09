be31089ce383c07b0c72b04b4df8476e40790055
Eric Lin <ericl160@nycstudents.net>
2024-02-09 15:34:57 -0500

    Starting

---
+++ Algorithms.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Algorithms {
+    public ArrayList<Integer> removeDuplicates(ArrayList<Integer>) {
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integerArrayList = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 4, 5));
+        Algorithms a = new Algorithms();
+        System.out.println("Remove Duplicates");
+    }
+}
