b167415932c3edca816dce08b65b42beabb69ad6
Eric Lin <ericl160@nycstudents.net>
2024-02-12 11:42:35 -0500

    Finished up to 7

--- Algorithms.java
+++ Algorithms.java
@@ -1,10 +1,55 @@
 import java.util.HashSet;
 import java.util.Set;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Algorithms {
-    public ArrayList<Integer> removeDuplicates(ArrayList<Integer>) {
-
+    public ArrayList<Integer> removeDuplicates(ArrayList<Integer> integers) {
+        Set<Integer> set = new HashSet<>(integers);
+        ArrayList<Integer> integers1 = new ArrayList<>(set);
+        return integers1;
+    }
+    public ArrayList<Integer> removeInstance(ArrayList<Integer> integers, int num) {
+        integers.removeIf(n -> n == num);
+        return integers;
+    }
+    public ArrayList<Integer> addAndSort(ArrayList<Integer> integers, int num) {
+        Collections.sort(integers);
+        for (int i = 0; i < integers.size(); i++) { // you could just add num into the list and then call Collections.sort(integers)
+            if (integers.get(i) > num) {
+                integers.add(i, num);
+                break;
+            }
+        }
+        return integers;
+    }
+    public boolean inReverseOrder(ArrayList<Integer> integers, ArrayList<Integer> reversed) {
+        if (integers.size() != reversed.size()) return false;
+       for (int i = 0; i < integers.size(); i++) {
+           if (integers.get(i) != reversed.get(reversed.size() - 1 - i)) {
+               return false;
+           }
+       }
+       return true;
+    }
+    public int maximumValue(ArrayList<Integer> integers) {
+        if (integers.isEmpty()) return -1;
+        int max = integers.get(0);
+        for (int i = 1; i < integers.size(); i++) {
+            if (integers.get(i) > max) {
+                max = integers.get(i);
+            }
+        }
+        return max;
+    }
+    public int minimumValue(ArrayList<Integer> integers) {
+        if (integers.isEmpty()) return -1;
+        int min = integers.get(0);
+        for (int i = 1; i < integers.size(); i++) {
+            if (integers.get(i) < min) {
+                min = integers.get(i);
+            }
+        }
+        return min;
     }
 }
--- Main.java
+++ Main.java
@@ -6,5 +6,29 @@ public class Main {
         ArrayList<Integer> integerArrayList = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 4, 5));
         Algorithms a = new Algorithms();
         System.out.println("Remove Duplicates");
+        System.out.println(a.removeDuplicates(new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 3))));
+        System.out.println(a.removeDuplicates(new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 1))));
+        System.out.println(a.removeDuplicates(new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 11))));
+        System.out.println("Remove Instances");
+        System.out.println(a.removeInstance(new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 11)), 1));
+        System.out.println(a.removeInstance(new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 11)), 1));
+        System.out.println(a.removeInstance(new ArrayList<>(Arrays.asList(1, 2, 2, 3, 3)), 2));
+        System.out.println("Add and Sort");
+        System.out.println(a.addAndSort(new ArrayList<>(Arrays.asList(1, 2, 2, 3, 3)), 4));
+        System.out.println(a.addAndSort(new ArrayList<>(Arrays.asList(1, 10, 100, 1000, 10000)), 50));
+        System.out.println(a.addAndSort(new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6)), 1));
+        System.out.println("In Reversed Order");
+        System.out.println(a.inReverseOrder(new ArrayList<>(Arrays.asList(1, 2, 3)), new ArrayList<>(Arrays.asList(3, 2, 1))));
+        System.out.println(a.inReverseOrder(new ArrayList<>(Arrays.asList(1, 2, 3)), new ArrayList<>(Arrays.asList(4, 3, 2, 1))));
+        System.out.println(a.inReverseOrder(new ArrayList<>(Arrays.asList(1, 2, 3)), new ArrayList<>(Arrays.asList(1, 2, 3))));
+        System.out.println("Largest Value");
+        System.out.println(a.maximumValue(new ArrayList<>(Arrays.asList(3, 2, 1))));
+        System.out.println(a.maximumValue(new ArrayList<>(Arrays.asList(1, 2, 7, 3))));
+        System.out.println(a.maximumValue(new ArrayList<>(Arrays.asList(10, 5, 0))));
+        System.out.println("Smallest Value");
+        System.out.println(a.minimumValue(new ArrayList<>(Arrays.asList(3, 2, 1))));
+        System.out.println(a.minimumValue(new ArrayList<>(Arrays.asList(1, 2, 7, 3))));
+        System.out.println(a.minimumValue(new ArrayList<>(Arrays.asList(10, 5, 0))));
+
     }
 }
